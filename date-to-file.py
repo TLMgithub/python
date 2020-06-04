836cff75a8dcf8708bc8db9c5e469e2accc99eb7
TLMgithub <51136967+TLMgithub@users.noreply.github.com>
2020-06-04 17:22:12 -0500

    Create date-to-file.py

---
+++ date-to-file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from datetime import datetime
+now = datetime.now() # current date and time
+date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+#print("date and time:",date_time)
+
+import sys
+original_stdout = sys.stdout # Save a reference to the original standard output
+with open('date.txt', 'w') as f:
+    sys.stdout = f # Change the standard output to the file we created.
+    print("date and time:",date_time)
+    sys.stdout = original_stdout # Reset the standard output to its original value
